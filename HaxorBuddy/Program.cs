458a5fe8da5c44db91e3b33546cb01530d553ec4
Pipe <pipernene@gmail.com>
2016-01-01 20:23:27 +0100

    Added Utils class

--- HaxorBuddy/Program.cs
+++ HaxorBuddy/Program.cs
@@ -15,6 +15,7 @@ namespace HaxorBuddy
         static void Main(string[] args)
         {
             Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+            
         }
 
         private static void Loading_OnLoadingComplete(EventArgs args)
