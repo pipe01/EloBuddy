343d1f5312068efbc0f0ce72eb0a80c2380b18aa
Pipe <pipernene@gmail.com>
2016-01-01 20:10:53 +0100

    Added loading message

--- HaxorBuddy/Program.cs
+++ HaxorBuddy/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Events;
+using System.Windows.Forms;
 
 namespace HaxorBuddy
 {
@@ -10,6 +14,12 @@ namespace HaxorBuddy
     {
         static void Main(string[] args)
         {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            Chat.Print("HaxorBuddy {0}", Application.ProductVersion);
         }
     }
 }
